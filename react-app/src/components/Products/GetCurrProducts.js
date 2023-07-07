0bc74b93d6b4e84a7b99485a804c2bb86c9fbed9
Vanessa <vxg026@gmail.com>
2023-07-06 19:19:48 -0700

    styled cart

--- react-app/src/components/Products/GetCurrProducts.js
+++ react-app/src/components/Products/GetCurrProducts.js
@@ -95,8 +95,21 @@ else{
             </div>
         ))}
         </div></div>
-          <div className="total-sum"><div className="div-sum"><h4>Merchendise sum total: </h4>${totalSum.toFixed(2)}</div>
+        <div>
+
+        </div>
+          <div className="total-sum">
+
+            <div className="shipping-details">
+                 <div className="div-sum"><p>Merchendise Subtotal: </p><h4>${totalSum.toFixed(2)}</h4></div>
+          <div className="sd1"><p>Shipping & Handling:</p> <h4>Free</h4></div>
+          <div className="sd1"><p>Estimated Tax & Other Fees </p><h4>{totalSum*.08.toFixed(2)}</h4></div>
+          <div className="sd1"><p>Total:</p><h4>{(totalSum+ totalSum*.08).toFixed(2)}</h4></div>
+            </div>
+
+
           <div><h5 className="limit-max">limit 10 per item*</h5></div>
+
         <div className="tota-checkout-btn">
 
  <button className="check-out-btn" onClick={handleCheckout}>Checkout Items</button>
