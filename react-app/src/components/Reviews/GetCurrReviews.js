0388dc74cd24b64437ad4c8c8538abd53a5a4c71
Vanessa <vxg026@gmail.com>
2023-07-06 18:47:56 -0700

    finished styling reviews

--- react-app/src/components/Reviews/GetCurrReviews.js
+++ react-app/src/components/Reviews/GetCurrReviews.js
@@ -37,27 +37,29 @@ if(!reviews) return ".."
     return (
         <div className="mng-review">
             <div>
-            <i className="fas fa-star"/>{(review?.star_rating)}
-            </div>
+
             {products && Object.values(products).filter(product=> product.id===review.product_id).map(results=>{
                 return(
                     <div>
                         <Link to={`/products/${results.id}`}>
-
-                        {results.name}
-                        </Link>
+  <div className="img-div-review">
                         <img src={results.image}/>
+                            </div>
+                        {/* {results.name} */}
+                        </Link>
+
                         </div>
                 )
             })
-
-
-
-
-}
-            <div>
+}</div>
+<div className="rating-review-txt-img">
+    <div>
+  <div className="rating-p-tag div">
             {review?.created_at}
             </div>
+            <p className="rating-p-tag">Rating: {(review?.star_rating)}<i className="fas fa-star"/></p>
+            </div>
+
             <div className="mng-review_text">
             {review?.review_text}
             </div>
@@ -76,7 +78,7 @@ if(!reviews) return ".."
             <div className="images-container-mng-review"><img className="images-mng-reviews"  src={review?.img4}onError={e=>{e.currentTarget.src="https://cdn.discordapp.com/attachments/1062942242450460744/1121263492327030874/Image_6-21-23_at_7.11_PM.jpeg"}}/></div>
                 }
             </div>
-
+            </div>
 
             <div className="buttons-div-mgn-rvws">
             <OpenModalButton
