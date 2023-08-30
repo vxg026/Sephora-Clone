a01c8b288c30fbf3f0b60b5da3f7db357f26f240
Vanessa <vxg026@gmail.com>
2023-08-30 16:57:45 -0700

    blog view posts done

--- react-app/src/components/Posts/PostHomePage.js
+++ react-app/src/components/Posts/PostHomePage.js
@@ -1,11 +1,16 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import GetCurrPosts from "./GetCurrPosts";
+import "./posts.css"
+import SearchQuestion from "./SearchQuestion";
 
 const PostHomePage = ()=>{
     return (
         <>
+        <SearchQuestion/>
+        <h4>All Posts</h4>
         <GetCurrPosts/>
+
         </>
     )
 }
